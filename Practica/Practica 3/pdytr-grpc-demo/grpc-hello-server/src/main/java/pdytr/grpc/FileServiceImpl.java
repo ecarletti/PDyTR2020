70df2ba1f882891a8a8064f315bb638e0cf436f5
esteban <estebandebole@gmail.com>
2020-10-31 16:06:42 -0300

    funcion de write
    Falta:
    * Retornar la cantidad de bytes escritos
    * Si el archivo existe amendear los bytes

--- Practica/Practica 3/pdytr-grpc-demo/grpc-hello-server/src/main/java/pdytr/grpc/FileServiceImpl.java
+++ Practica/Practica 3/pdytr-grpc-demo/grpc-hello-server/src/main/java/pdytr/grpc/FileServiceImpl.java
@@ -16,17 +16,18 @@ public class FileServiceImpl extends FileServiceGrpc.FileServiceImplBase {
 	private BufferedOutputStream mBufferedOutputStream = null;
 
 	@Override
-	public StreamObserver<ReadRequest> readFile(final StreamObserver<ReadResponse> responseObserver) {
-		return new StreamObserver<ReadRequest>() {
+	public StreamObserver<WriteRequest> writeFile(final StreamObserver<WriteResponse> responseObserver) {
+		return new StreamObserver<WriteRequest>() {
 			int mmCount = 0;
 
 			@Override
-			public void onNext(ReadRequest request) {
+			public void onNext(WriteRequest request) {
 				mmCount++;
 
 				byte[] data = request.getData().toByteArray();
 				long offset = request.getOffset();
-				String name = request.getName();
+				String src = request.getSrc();
+				String dest = request.getDest();
 				try {
 					if (mBufferedOutputStream == null) {
 						// timestamp for name of receive file
@@ -35,7 +36,7 @@ public class FileServiceImpl extends FileServiceGrpc.FileServiceImplBase {
 						Files.createDirectories(Paths.get("store"));
 						// store de receive file
 						mBufferedOutputStream = new BufferedOutputStream(
-								new FileOutputStream("store/receive_" + timeStamp));
+								new FileOutputStream("store/" + dest));
 					}
 					mBufferedOutputStream.write(data);
 					mBufferedOutputStream.flush();
@@ -51,7 +52,7 @@ public class FileServiceImpl extends FileServiceGrpc.FileServiceImplBase {
 
 			@Override
 			public void onCompleted() {
-				responseObserver.onNext(ReadResponse.newBuilder().setStatus(mStatus).setMessage(mMessage).build());
+				responseObserver.onNext(WriteResponse.newBuilder().setStatus(mStatus).setMessage(mMessage).build());
 				responseObserver.onCompleted();
 				if (mBufferedOutputStream != null) {
 					try {
--- Practica/Practica 3/pdytr-grpc-demo/grpc-hello-server/src/main/java/pdytr/grpc/UploadFileClient.java
+++ Practica/Practica 3/pdytr-grpc-demo/grpc-hello-server/src/main/java/pdytr/grpc/UploadFileClient.java
@@ -10,6 +10,8 @@ import java.util.logging.Logger;
 
 import com.google.protobuf.ByteString;
 
+import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -17,6 +19,13 @@ import io.grpc.stub.StreamObserver;
 public class UploadFileClient {
 	private static final Logger logger = Logger.getLogger(UploadFileClient.class.getName());
 	private static final int PORT = 50051;
+	public static String src = "";
+	public static String dest = "";
+	public static String host = "";
+	public static Boolean isSetBytesFlag = false;
+	public static Boolean listView = false;
+	public static Integer bytes = 0;
+	public static Integer initialPosition = 0;
 
 	private final ManagedChannel mChannel;
 	private final FileServiceGrpc.FileServiceBlockingStub mBlockingStub;
@@ -40,12 +49,12 @@ public class UploadFileClient {
 		mChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
 	}
 
-	public void startStream(final String filepath) {
-		logger.info("tid: " + Thread.currentThread().getId() + ", Will try to readFile");
-		StreamObserver<ReadResponse> responseObserver = new StreamObserver<ReadResponse>() {
+	public void startStream(final String src, final String dest) {
+		logger.info("tid: " + Thread.currentThread().getId() + ", Will try to writeFile");
+		StreamObserver<WriteResponse> responseObserver = new StreamObserver<WriteResponse>() {
 
 			@Override
-			public void onNext(ReadResponse value) {
+			public void onNext(WriteResponse value) {
 				logger.info("Client response onNext");
 			}
 
@@ -60,10 +69,10 @@ public class UploadFileClient {
 			}
 		};
 
-		StreamObserver<ReadRequest> requestObserver = mAsyncStub.readFile(responseObserver);
+		StreamObserver<WriteRequest> requestObserver = mAsyncStub.writeFile(responseObserver);
 
 		try {
-			File file = new File(filepath);
+			File file = new File(src);
 			if (file.exists() == false) {
 				logger.info("File does not exist");
 				return;
@@ -75,7 +84,7 @@ public class UploadFileClient {
 				int size = 0;
 				while ((size = bInputStream.read(buffer)) > 0) {
 					ByteString byteString = ByteString.copyFrom(buffer, 0, size);
-					ReadRequest req = ReadRequest.newBuilder().setName(filepath).setData(byteString).setOffset(size)
+					WriteRequest req = WriteRequest.newBuilder().setSrc(src).setDest(dest).setData(byteString).setOffset(size)
 							.build();
 					requestObserver.onNext(req);
 				}
@@ -91,11 +100,152 @@ public class UploadFileClient {
 		requestObserver.onCompleted();
 	}
 
-	public static void main(String[] args) throws Exception {
-		UploadFileClient client = new UploadFileClient("localhost", PORT);
+	public static void initParams(String[] argv) {
+		int c;
+		String arg;
+		LongOpt[] longopts = new LongOpt[8];
+		StringBuffer listViewSb = new StringBuffer("0");
+
+		longopts[0] = new LongOpt("all", LongOpt.NO_ARGUMENT, listViewSb, '1');
+		longopts[1] = new LongOpt("bytes", LongOpt.REQUIRED_ARGUMENT, null, 'a');
+		longopts[2] = new LongOpt("pos", LongOpt.REQUIRED_ARGUMENT, null, 'p');
+		longopts[3] = new LongOpt("host", LongOpt.REQUIRED_ARGUMENT, null, 'h');
+		longopts[4] = new LongOpt("src", LongOpt.REQUIRED_ARGUMENT, null, 's');
+		longopts[5] = new LongOpt("dest", LongOpt.REQUIRED_ARGUMENT, null, 'd');
+
+		Getopt g = new Getopt("flags", argv, "vbls:d:h:a:p:", longopts);
+		g.setOpterr(false);
+
+		while ((c = g.getopt()) != -1) {
+			switch (c) {
+			case 0:
+				/* If this option set a flag, do nothing else now. */
+				arg = g.getOptarg();
+				if (new Integer(longopts[g.getLongind()].getFlag().toString()) != 0)
+					break;
+
+				System.out.println("option %s" + longopts[g.getLongind()].getName() + " with arg %s"
+						+ ((arg != null) ? arg : "null"));
+				break;
+			case 'l':
+				listViewSb = new StringBuffer("1");
+				break;
+			case 's':
+				src = g.getOptarg();
+				break;
+			case 'd':
+				dest = g.getOptarg();
+				break;
+			case 'h':
+				host = g.getOptarg();
+				break;
+			case 'a':
+				isSetBytesFlag = true;
+				bytes = new Integer(g.getOptarg());
+				break;
+			case 'p':
+				initialPosition = new Integer(g.getOptarg());
+				break;
+			case '?':
+				/* getopt_long already printed an error message. */
+				break;
+			default:
+				System.exit(0);
+			}
+		}
+
+		listViewSb.trimToSize();
+
+		listView = new Integer(listViewSb.toString()) != 0;
+
+		for (int i = g.getOptind() + 1; i < argv.length; i++) {
+			System.out.println("Non option argv element: " + argv[i] + "\n");
+		}
+	}
+
+	public static void main(String[] argv) throws Exception {
+		long startTime, endTime;
+
+		/* Look for hostname and msg length in the command line */
+		if (argv.length < 1) {
+			System.out.printf("Utilizacion: cliente\n"
+					+ "\t- write or add --src <local> --dest <remote>: Agrega un archivo de <local> a <remote> \n"
+					+ "\t- read or get --dest <local> --src <remote>: Almacene un archivo de <local> en <remote>\n"
+					+ "\t- ls or list --src <remote/directory>: List files from <remote/directory>\n"
+					+ "\t- rw or readwrite --src <local> --dest <remote>: Almacena un archivo de <local> a <remote> y realiza una copia de seguridad en el servidor \n");
+			System.exit(1);
+		}
+
+		String command = argv[0];
+		initParams(argv);
+
+		if (src.isEmpty()) {
+			String[] commandsWithSrc = { "write", "add", "read", "get" };
+
+			for (String commandWithSrc : commandsWithSrc) {
+				if (command.equals(commandWithSrc)) {
+					System.err.println("Error: Especifique una ruta --src");
+					System.exit(1);
+				}
+			}
+		}
+		// el archivo destino se llama por default tmp1
+		if (dest.isEmpty()) {
+			System.err.println("Advertencia: --dest fue setteado a tmp1");
+
+			dest = "tmp1";
+		}
+		// setea localhost como host por default localhost
+		if (host.isEmpty())
+			host = "localhost";
+		UploadFileClient client = new UploadFileClient(host, PORT);
 		try {
-			client.startStream(args[0]);
-			logger.info("Done with startStream");
+			switch (command) {
+			case "read":
+			case "get":
+				startTime = System.nanoTime();
+				// read(remote);
+				endTime = System.nanoTime();
+
+				System.out.printf("Tomo: %d ms\n", (endTime - startTime) / 1000000);
+				break;
+
+			case "write":
+			case "add":
+				startTime = System.nanoTime();
+				client.startStream(src, dest);
+				endTime = System.nanoTime();
+
+				System.out.printf("Tomo: %d ms\n", (endTime - startTime) / 1000000);
+				break;
+
+			case "time":
+				startTime = System.nanoTime();
+				// remote.time();
+				endTime = System.nanoTime();
+
+				System.out.println(endTime - startTime);
+				break;
+
+			case "timeout":
+				System.out.println("Ejecutando el comando: timeout...");
+
+				// Boolean ret = remote.timeout();
+
+				// if (ret)
+				// System.out.println("Completado!");
+				// else
+				// System.out.println("Timeout excedido");
+
+				break;
+
+			default:
+				System.err.println("Comando no disponible");
+				break;
+			}
+		} catch (Exception e) {
+			System.err.println("Excepcion general!");
+			e.printStackTrace();
 		} finally {
 			try {
 				Thread.sleep(500);
